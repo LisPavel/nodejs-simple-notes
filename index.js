5d1b8fa35873e237d1dba6c87cecea5f8facac41
Pavel Lisitsyn <pavellisitsyn69@gmail.com>
2022-07-23 15:22:22 +0300

    post method

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require("http");
 const fs = require("fs/promises");
 const path = require("path");
 
+const { addNote } = require("./notes.controller");
+
 const PORT = 3000;
 
 const pagesPath = path.resolve(__dirname, "pages");
@@ -16,6 +18,21 @@ const server = http.createServer(async (req, res) => {
             "Content-Type": "text/html",
         });
         res.end(content);
+    } else if (req.method === "POST") {
+        const body = [];
+
+        res.writeHead(200, "Content-Type: text/plain; charset: utf-8");
+
+        req.on("data", (data) => {
+            body.push(Buffer.from(data));
+        });
+
+        req.on("end", async () => {
+            const title = body.toString().split("=")[1].replace(/\+/g, " ");
+            await addNote(title);
+
+            res.end(`success ${title}`);
+        });
     }
 });
 
